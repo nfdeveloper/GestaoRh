9bbda0cb824c28194f571f1471dbef4a0818f25b
nfdeveloper <nfdeveloper04@gmail.com>
2023-08-15 23:47:03 -0300

    Implementando listagem de funcionarios

--- apps/funcionarios/urls.py
+++ apps/funcionarios/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path, include
-from .views import home
+from django.urls import path
+from .views import FuncionariosList
 
 urlpatterns = [
-    path('', home),
+    path('', FuncionariosList.as_view(), name='list_funcionarios'),
 ]
