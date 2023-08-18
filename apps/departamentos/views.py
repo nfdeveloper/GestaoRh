6da3ee596efdd7fdf2985b38106b7c0021e50b66
nfdeveloper <nfdeveloper04@gmail.com>
2023-08-17 22:34:41 -0300

    Deletando Departamento

--- apps/departamentos/views.py
+++ apps/departamentos/views.py
@@ -1,5 +1,11 @@
-from django.views.generic import ListView, CreateView, UpdateView
+from django.views.generic import (
+    ListView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
 from .models import Departamento
+from django.urls import reverse_lazy
 
 class DepartamentosList(ListView):
     model = Departamento
@@ -22,3 +28,8 @@ class DepartamentoCreate(CreateView):
 class DepartamentoUpdate(UpdateView):
     model = Departamento
     fields = ['nome']
+
+
+class DepartamentoDelete(DeleteView):
+    model = Departamento
+    success_url = reverse_lazy('list_departamentos')
