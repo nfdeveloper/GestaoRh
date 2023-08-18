e95cd6e442331029864b39a5328e26f33e7da88a
nfdeveloper <nfdeveloper04@gmail.com>
2023-08-17 22:03:10 -0300

    Criando Departamento

--- apps/departamentos/views.py
+++ apps/departamentos/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 from .models import Departamento
 
 class DepartamentosList(ListView):
@@ -8,3 +7,14 @@ class DepartamentosList(ListView):
     def get_queryset(self):
         empresa_logada = self.request.user.funcionario.empresa
         return Departamento.objects.filter(empresa=empresa_logada)
+
+
+class DepartamentoCreate(CreateView):
+    model= Departamento
+    fields = ['nome']
+
+    def form_valid(self, form):
+        departamento = form.save(commit=False)
+        departamento.empresa = self.request.user.funcionario.empresa
+        departamento.save()
+        return super(DepartamentoCreate, self).form_valid(form)
